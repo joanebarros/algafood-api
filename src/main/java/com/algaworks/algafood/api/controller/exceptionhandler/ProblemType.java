e3e07752a1d477d260dc37b7a7e82bab1b1ef060
joanebarros <joane.barros@hotmail.com>
2026-09-02 17:28:39 -0300

    feat: Customizando exception handlers de ResponseEntityExceptionHandler

---
+++ src/main/java/com/algaworks/algafood/api/controller/exceptionhandler/ProblemType.java
@@ -0,0 +1,21 @@
+package com.algaworks.algafood.api.controller.exceptionhandler;
+
+import lombok.Getter;
+
+@Getter
+public enum ProblemType {
+
+  ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
+  ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
+  ERRO_NEGOCIO("/erro-negocio", "Erro de negócio");
+
+  private String title;
+  private String uri;
+
+  ProblemType(String path, String title) {
+    this.uri = "https://algafood.com.br" + path;
+    this.title = title;
+  }
+
+
+}
